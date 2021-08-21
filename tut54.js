98f059748f81042f9d954ef872f0bf302d316358
Kunal Joshi <57366074+Kunal352000@users.noreply.github.com>
2021-08-21 18:52:23 +0530

    Add files via upload

---
+++ tut54.js
@@ -0,0 +1,8 @@
+let name="harry";
+let name1="shubham";
+let name2="Rohan";
+let name3="sagar";
+console.log(name + "is a good boy");
+console.log(name1 + "is a good boy");
+console.log(name2 + "is a good boy");
+console.log(name3 + "is a good boy");
---
+++ tut55.js
@@ -0,0 +1,12 @@
+function greet(name){
+    console.log(name + "is a good boy");
+}
+let name="Harry";
+let name1="shubham";
+let name2="Rohan";
+let name3="sagar";
+
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
